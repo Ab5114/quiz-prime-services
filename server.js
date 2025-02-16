120b8385cf2a3ab5673c64fd7438e5584e1bf14d
Ab5114 <sharmanitin5114@gmail.com>
2025-02-16 19:27:59 +0530

    cross origin

--- server.js
+++ server.js
@@ -11,12 +11,26 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 
 app.use(cookieParser());
+const allowedOrigins = [
+  "https://quizzy-prime.vercel.app", // Production frontend
+  "http://localhost:3000", // Localhost for development
+];
+
 app.use(
   cors({
-    origin: "*", // Allow all origins
-    credentials: true, // Allow sending cookies
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        // Allow requests from allowed origins or if there is no origin (e.g., Postman)
+        callback(null, true);
+      } else {
+        // Block any origin that is not allowed
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow cookies/credentials
   })
 );
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
