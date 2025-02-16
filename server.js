baa70393d1453d73553cf2be018ab16340b4c838
Ab5114 <sharmanitin5114@gmail.com>
2025-02-16 18:24:42 +0530

    cross origin

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "http://localhost:3001", 
+    credentials: true,  
+  })
+);
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
