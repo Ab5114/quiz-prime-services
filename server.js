ee7e555738d3185a03c09055f814073cab6e9846
Ab5114 <sharmanitin5114@gmail.com>
2025-02-16 18:19:32 +0530

    cross origin

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 // server.js
 require("dotenv").config();
- 
 
 const express = require("express");
 const app = express();
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 const userRoutes = require("./routes/user");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
- 
 
-
- 
 app.use(express.json());
 
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3001",  
-    credentials: true,  
-  })
-);
+app.use(cors());
 
-
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB", err));
 
- 
-app.use("/user" ,userRoutes);
+app.use("/user", userRoutes);
 
- 
-const PORT=process.env.PORT || 5000;
- app.listen(process.env.PORT,()=>console.log(`Server running on PORT ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on PORT ${PORT}`)
+);
