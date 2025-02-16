dc421161a39f4928611e1ef041dcd98e2fe611c0
Ab5114 <sharmanitin5114@gmail.com>
2025-02-16 16:39:54 +0530

    Updated env

--- server.js
+++ server.js
@@ -31,6 +31,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/user",userRoutes);
 
  
-
-const PORT=5025;
-app.listen(PORT,()=>console.log(`Server running on PORT ${PORT}`));
+const PORT=process.env.PORT || 5000;
+ app.listen(process.env.PORT,()=>console.log(`Server running on PORT ${PORT}`));
